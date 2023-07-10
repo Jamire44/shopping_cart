9b5674e6ab0fbd8c750a0dfd92e49a8b3598ebe2
Jamire44 <2003doyle@gmail.com>
2023-07-10 01:29:46 +0100

    Added lists and for loops

---
+++ main.py
@@ -0,0 +1,25 @@
+# Shopping cart program
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+
+print("\n------ YOUR CART -----")
+
+for food in foods:
+    print(f"${food}")
+    
+for price in prices:
+    total += price
+    
+print(f"Your total is ${total}")
